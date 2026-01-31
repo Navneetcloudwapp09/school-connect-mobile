1e3e094e6b6ce160fb8db9a57ecd6b04f63cc4b2
navneetpatidar <navneet.cloudwapp@gmail.com>
2026-01-31 10:57:58 +0530

    31 Jan 2026 || 10: 57 PM || api

--- schoolconnect/lib/constants/ApiServer.dart
+++ schoolconnect/lib/constants/ApiServer.dart
@@ -2,4 +2,6 @@ class Apiserver {
   static const String baseurl =
       "https://school-connect-mgmt-backend.vercel.app";
   static const String loginteacher = baseurl + "/api/teacher/auth/login";
+  static const String attendanceclass =
+      baseurl + "/api/attendance/class/99565b63-e04e-41a7-869a-dc8c72a051f7";
 }
